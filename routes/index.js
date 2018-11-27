31e1d9e2d126b83006eeb0b0e85886a8edbe9ab5
ItachiUchiha1998 <vinayak.shrivastava@gmail.com>
2018-11-28 04:57:44 +0530

    S3 setup done

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 const db = require('../db/model');
 const path = require('path');
 const cors = require('cors');
+const multerS3 = require('multer-s3');
+const multer = require('multer')
+const AWS = require('aws-sdk');
 const Blockcluster = require('blockcluster');
 
 router.use(bodyParser.urlencoded({ extended: true }))
@@ -19,6 +22,31 @@ router.use(function(req, res, next) {
     next();
 });
 router.use(cors());
+
+const aws_access_key_id = "AKIAICV5P2PGKZ2JF26Q"
+const aws_secret_access_key = "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"
+
+AWS.config.update({
+    accessKeyId: aws_access_key_id,
+    secretAccessKey: aws_secret_access_key,
+    region: 'us-east-2'
+});
+
+const s3 = new AWS.S3();
+
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'frontrow-blockcluster',
+        key: function(req, file, cb) {
+            cb(null, Date.now() + '-' + file.originalname)
+        }
+    })
+})
+
+router.post('/api/upload', upload.single('video') , (req, res) => { 
+    res.send({ path: req.file.location })
+})
 
 router.post('*',(req,res) => {
     res.status(404).send({success: false,message: 'Page does not exist!'});

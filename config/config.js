9f242be5ffebc2ce2f8a3fa61c091c480efcc2a1
Narayan Prusty <arsenal.narayan@gmail.com>
2019-06-03 15:02:51 -0700

    added aws config

--- config/config.js
+++ config/config.js
@@ -9,6 +9,10 @@ var config = {
     DB: {
         connectionString: process.env.MONGO_URL || "mongodb://localhost/frontrow",
     },
+    AWS: {	
+        aws_access_key_id: "AKIA5XOL3DOJQ5ZDDPXM",	
+        aws_secret_access_key: "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"	
+    },
 };
 
 module.exports = config;

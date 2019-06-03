d3647647d8322bbf1a4e1e07a382841d2a456640
Narayan Prusty <arsenal.narayan@gmail.com>
2019-06-03 14:52:48 -0700

    production setting

--- config/config.js
+++ config/config.js
@@ -1,17 +1,13 @@
 var config = {
     BLOCKCLUSTER: {
         locationDomain: "app-ap-south-1b.blockcluster.io",
-        instanceId: 'sbtixarqpa'
-    },
-    AWS: {
-        aws_access_key_id: "AKIAICV5P2PGKZ2JF26Q",
-        aws_secret_access_key: "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"
+        instanceId: process.env.BC_INSTANCE_ID || 'sbtixarqpa'
     },
     JWTSecret: {
         secret: "asdfgh",
     },
     DB: {
-        connectionString: "mongodb://localhost/frontrow",
+        connectionString: process.env.MONGO_URL || "mongodb://localhost/frontrow",
     },
 };
 
